d0c5d484728eb5238202f2e883adc11538dde73d
DESKTOP-TIDRHG5\ruize <nalanchangge@gmail.com>
2021-10-09 10:50:25 +0200

    First commit

--- src/model/Camerieri.java
+++ src/model/Camerieri.java
@@ -5,5 +5,14 @@ package model;
  * @data 09/10/2021
  * @project Progetto_ristorante
  */
-public class Camerieri {
+public class Camerieri implements Runnable{
+    public String nome;
+    public Camerieri(String nome){
+        this.nome = nome;
+
+    }
+    @Override
+    public void run() {
+
+    }
 }
--- src/model/Cucina.java
+++ src/model/Cucina.java
@@ -5,5 +5,15 @@ package model;
  * @data 09/10/2021
  * @project Progetto_ristorante
  */
-public class Cucina {
+public class Cucina implements Runnable{
+    private String nome;
+    private String piatto;
+
+    public Cucina(String nome){
+        this.nome = nome;
+    }
+    @Override
+    public void run() {
+
+    }
 }
